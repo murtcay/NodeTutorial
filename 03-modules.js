14a258bcbb821c192d6c6c3969fece509257bd94
Murat ÇAY <murtcay@gmail.com>
2021-12-02 13:22:05 +0300

    node tutorial: modules setup and first module.

---
+++ 03-modules.js
@@ -0,0 +1,10 @@
+// CommonJS, every file is module (by default)
+// Modules - Encapsulated Code (only share minimum)
+
+const names = require('./04-names');
+const sayHi = require('./05-utils');
+
+console.log(names);
+sayHi('Anna');
+sayHi(names.john);
+sayHi(names.peter);

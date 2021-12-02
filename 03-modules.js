a2abdf8cd88851421ec9101820f9293a16029eaa
Murat ÇAY <murtcay@gmail.com>
2021-12-02 13:36:01 +0300

    node tutorial: mind grenade.

--- 03-modules.js
+++ 03-modules.js
@@ -4,8 +4,7 @@
 const names = require('./04-names');
 const sayHi = require('./05-utils');
 const data = require('./06-alternative-flavor');
-
-console.log(data);
+require('./07-mind-grenade');
 
 sayHi('Anna');
 sayHi(names.john);

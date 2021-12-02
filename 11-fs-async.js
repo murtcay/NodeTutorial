caab928005658d278765480e73c310d4b5844d70
Murat ÇAY <murtcay@gmail.com>
2021-12-02 14:24:47 +0300

    node tutorial: fs module - async with callback hell

---
+++ 11-fs-async.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+
+fs.readFile('./content/first.txt', 'utf8', (err, result) => {
+  if(err) {
+    console.log(err.message);
+    return;
+  }
+
+  const first = result;
+  fs.readFile('./content/second.txt', 'utf8', (err, result) => {
+    if(err) {
+      console.log(err.message);
+      return;
+    }
+
+    const second = result;
+
+    fs.writeFile(
+      './content/result-async.txt', 
+      `Here is the result: ${first}, ${second}`, 
+      (err, result) => {
+        if(err) {
+          console.log(err.message);
+          return;
+        }
+        console.log(result);
+      }
+    );
+  });
+});
